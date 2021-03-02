f5d3db67f97d34682a38aef5b6697880d8003f62
Sergiy <sergey.morenets@gmail.com>
2021-03-02 20:09:33 +0200

    Change #9

--- quarkus/src/main/java/quarkus/LibraryResource.java
+++ quarkus/src/main/java/quarkus/LibraryResource.java
@@ -1,7 +1,9 @@
 package quarkus;
 
+import org.eclipse.microprofile.config.Config;
 import org.eclipse.microprofile.config.inject.ConfigProperty;
 
+import javax.inject.Inject;
 import javax.ws.rs.GET;
 import javax.ws.rs.Path;
 import javax.ws.rs.Produces;
@@ -10,12 +12,15 @@ import javax.ws.rs.core.MediaType;
 @Path("/library")
 public class LibraryResource {
 
-    @ConfigProperty(name = "library.name")
-    private String libraryName;
+//    @ConfigProperty(name = "library.name")
+//    private String libraryName;
+
+    @Inject
+    private Config config;
 
     @GET
     @Produces(MediaType.TEXT_PLAIN)
     public String libraryName() {
-        return libraryName;
+        return config.getValue("library.name", String.class);
     }
 }
