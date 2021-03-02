05726aef2d748f1a41a72e9907082ef3f0b7ee84
Sergiy <sergey.morenets@gmail.com>
2021-03-02 20:27:06 +0200

    Change #16

--- micronaut/src/main/java/micronaut/controller/LibraryController.java
+++ micronaut/src/main/java/micronaut/controller/LibraryController.java
@@ -1,5 +1,6 @@
 package micronaut.controller;
 
+import io.micronaut.context.annotation.Property;
 import io.micronaut.context.annotation.Value;
 import io.micronaut.http.MediaType;
 import io.micronaut.http.annotation.Controller;
@@ -8,7 +9,8 @@ import io.micronaut.http.annotation.Get;
 @Controller("/library")
 public class LibraryController {
 
-    @Value("${library.name}")
+    //@Value("${library.name}")
+    @Property(name = "library.name")
     private String libraryName;
 
     @Get(produces = MediaType.TEXT_PLAIN)
