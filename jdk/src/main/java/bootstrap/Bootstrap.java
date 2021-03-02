0cc2861680294891d3efe57e894b07c15f466ccd
Sergiy <sergey.morenets@gmail.com>
2021-03-02 19:41:16 +0200

    Change #2

--- jdk/src/main/java/bootstrap/Bootstrap.java
+++ jdk/src/main/java/bootstrap/Bootstrap.java
@@ -2,13 +2,16 @@ package bootstrap;
 
 import java.io.IOException;
 import java.io.InputStream;
+import java.nio.file.Files;
+import java.nio.file.Path;
 import java.util.Properties;
 
 public class Bootstrap {
     public static void main(String[] args) throws IOException {
         Properties properties = new Properties();
+        Path path = Path.of("jdk/src/main/resources/application.properties");
 
-        try(InputStream in = Bootstrap.class.getClassLoader().getResourceAsStream("application.properties")) {
+        try(InputStream in = Files.newInputStream(path)) {
             properties.load(in);
             System.out.println("Library name is " + properties.getProperty("library.name", "N/A"));
         }
