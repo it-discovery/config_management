df900f25e1391c6972aefe441404f34c748dff9a
Sergiy <sergey.morenets@gmail.com>
2021-03-02 20:54:47 +0200

    Change #23

--- spring/src/main/java/spring/web/LibraryConfiguration.java
+++ spring/src/main/java/spring/web/LibraryConfiguration.java
@@ -5,10 +5,17 @@ import lombok.Setter;
 import org.springframework.boot.context.properties.ConfigurationProperties;
 import org.springframework.stereotype.Component;
 
+import java.util.List;
+import java.util.Map;
+
 @ConfigurationProperties("library")
 @Component
 @Getter
 @Setter
 public class LibraryConfiguration {
     private String name;
+
+    private List<String> names;
+
+    private Map<String, String> config;
 }
