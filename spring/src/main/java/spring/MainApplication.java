20bdff8cd3ce6ba8299aaa11e522546130d1259b
Sergiy <sergey.morenets@gmail.com>
2021-03-02 20:39:10 +0200

    Change #19

--- spring/src/main/java/spring/MainApplication.java
+++ spring/src/main/java/spring/MainApplication.java
@@ -2,8 +2,10 @@ package spring;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.PropertySource;
 
 @SpringBootApplication
+@PropertySource("application.json")
 public class MainApplication {
     public static void main(String[] args) {
         SpringApplication.run(MainApplication.class, args);
